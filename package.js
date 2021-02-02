f3bae945da97090bac547d228d440ed1596e51ac
Yacine Merzouk <yacine@merzouk.ca>
2021-02-02 11:17:19 -1000

    1.0.4: Updated for Meteor 2.0 and jquery 3.0.0

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
     name: 'meteorhubdotnet:bureaucrat',
     summary: 'The bureacrat processes your forms and returns tidy objects.',
-    version: '1.0.3',
+    version: '1.0.4',
     git: 'https://github.com/meteorhubdotnet/bureaucrat-meteor-package'
 });
 
 Package.onUse(function(api) {
-    api.versionsFrom('1.8.1');
-    api.use(['jquery@1.11.11'], 'client');
+    api.versionsFrom('METEOR@2.0');
+    api.use(['jquery@3.0.0'], 'client');
     api.addFiles(['bureaucrat.js'], 'client');
     api.export('Bureaucrat');
 });
